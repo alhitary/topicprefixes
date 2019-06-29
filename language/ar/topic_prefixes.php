1ec9e82772ca9918c83d08a149c1883bb5634729
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2019-06-29 23:08:56 +0300

    Create topic_prefixes.php

---
+++ language/ar/topic_prefixes.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * Topic Prefixes extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * Translated By : Bassel Taha Alhitary <http://alhitary.net>
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'TOPIC_PREFIX'			=> 'بادئة الموضوع',
+));
